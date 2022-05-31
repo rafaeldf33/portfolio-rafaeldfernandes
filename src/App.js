ebe0ca10c0d76ea4b27fed19c112b9a729da682a
Rafael Damasceno <rafaeldf33@gmail.com>
2022-05-31 00:06:33 -0300

    primeiro deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Intro from './components/Intro';
+import About from './components/About';
 
 function App() {
   return (
 
     <div className="App">
-      <Intro nome="Seu nome" cargo1="Analista de Sistema"/>      
+      <Intro nome="Seu nome" cargo1="Analista de Sistema"/>
+      <About />      
     </div>
   );
 }
